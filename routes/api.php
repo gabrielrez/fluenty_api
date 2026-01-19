131a47908c4250d3f2f4e54712c0910125f2fc50
Gabrielrez <gabrielrezcpessoa@gmail.com>
2026-01-19 16:05:18 -0300

    feat: user profile endpoint implemented

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', fn() => response()->json('pong'));
@@ -8,8 +9,11 @@ Route::get('/ping', fn() => response()->json('pong'));
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/auth/check', fn() => response()->json(true));
+
+    Route::get('/users/profile', [UserController::class, 'profile']);
 });
