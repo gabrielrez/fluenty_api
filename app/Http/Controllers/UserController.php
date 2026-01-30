5dc02e5c612b4c93c7cbc13df2180bc942eb8aff
Gabrielrez <gabrielrezcpessoa@gmail.com>
2026-01-30 13:56:59 -0300

    feat: implement UserResource for user profile response formatting

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function profile(Request $request)
     {
-        return $this->respond([
-            'user' => $request->user(),
-        ]);
+        return $this->respond(new UserResource($request->user()));
     }
 }
