4e3f22257172bb08bf25936d32c222689fa0ea68
Gabrielrez <gabrielrezcpessoa@gmail.com>
2026-04-20 10:51:42 -0300

    feat: update LessonSeeder to include is_free field for lessons

--- database/seeders/LessonSeeder.php
+++ database/seeders/LessonSeeder.php
@@ -23,6 +23,7 @@ class LessonSeeder extends Seeder
                 "level" => LessonLevelEnum::Beginner,
                 "category_id" => 1,
                 "source" => "The Wonderful Wizard of Oz",
+                "is_free" => true
             ],
             [
                 "title" => "My First Home as a Horse",
@@ -34,7 +35,8 @@ class LessonSeeder extends Seeder
                 "duration" => 51,
                 "level" => LessonLevelEnum::Beginner,
                 "category_id" => 1,
-                "source" => "Black Beauty"
+                "source" => "Black Beauty",
+                "is_free" => false
             ],
             [
                 "title" => "White Rabbit",
@@ -46,7 +48,8 @@ class LessonSeeder extends Seeder
                 "duration" => 30,
                 "level" => LessonLevelEnum::Beginner,
                 "category_id" => 1,
-                "source" => "Alice's Adventures in Wonderland"
+                "source" => "Alice's Adventures in Wonderland",
+                "is_free" => false
             ],
             [
                 "title" => "The Dashwood Family at Norland Park",
@@ -58,7 +61,8 @@ class LessonSeeder extends Seeder
                 "duration" => 53,
                 "level" => LessonLevelEnum::Intermediate,
                 "category_id" => 1,
-                "source" => "Sense and Sensibility"
+                "source" => "Sense and Sensibility",
+                "is_free" => true
             ],
             [
                 "title" => "A Quiet Escape",
@@ -70,19 +74,60 @@ class LessonSeeder extends Seeder
                 "duration" => 40,
                 "level" => LessonLevelEnum::Advanced,
                 "category_id" => 1,
-                "source" => "Crime and Punishment"
+                "source" => "Crime and Punishment",
+                "is_free" => false
             ],
             [
                 "title" => "Call me Ishmael",
                 "description" => "Some years ago—never mind how long precisely...",
-                "text" => "Call me Ishmael.\n\nSome years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.\n\nIt is a way I have of driving off the spleen and regulating the circulation.\n\nWhenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet;\n\nand especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—\n\nthen, I account it high time to get to sea as soon as I can.\n\nThis is my substitute for pistol and ball.\n\nWith a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship.\n\nThere is nothing surprising in this.\n\nIf they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.",
-                "translation" => "Chame-me Ishmael.\n\nHá alguns anos—não importa exatamente quanto tempo—tendo pouco ou nenhum dinheiro na bolsa, e nada em particular que me interessasse em terra, pensei que navegaria um pouco e veria a parte aquática do mundo.\n\nÉ um modo que tenho de afastar a melancolia e regular a circulação.\n\nSempre que me vejo ficando carrancudo ao redor da boca; sempre que é um novembro úmido e chuvoso em minha alma; sempre que me pego involuntariamente parando diante de armazéns de caixões e seguindo atrás de todos os funerais que encontro;\n\ne especialmente sempre que meus humores sombrios assumem tal domínio sobre mim, que é necessário um forte princípio moral para impedir-me de deliberadamente entrar na rua e metodicamente derrubar os chapéus das pessoas—\n\nentão considero que é mais do que hora de ir para o mar o mais rápido possível.\n\nEste é o meu substituto para pistola e bala.\n\nCom um floreio filosófico, Catão lança-se sobre sua espada; eu, tranquilamente, tomo o navio.\n\nNão há nada de surpreendente nisso.\n\nSe apenas o soubessem, quase todos os homens, em algum grau, em um momento ou outro, nutrem sentimentos muito semelhantes aos meus em relação ao oceano.",
+                "text" => "Call me Ishmael.\n\nSome years ago, never mind how long precisely, having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.\n\nIt is a way I have of driving off the spleen and regulating the circulation.\n\nWhenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet;\n\nand especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—\n\nthen, I account it high time to get to sea as soon as I can.\n\nThis is my substitute for pistol and ball.\n\nWith a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship.\n\nThere is nothing surprising in this.\n\nIf they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.",
+                "translation" => "Chame-me Ishmael.\n\nHá alguns anos, não importa exatamente quanto tempo, tendo pouco ou nenhum dinheiro na bolsa, e nada em particular que me interessasse em terra, pensei que navegaria um pouco e veria a parte aquática do mundo.\n\nÉ um modo que tenho de afastar a melancolia e regular a circulação.\n\nSempre que me vejo ficando carrancudo ao redor da boca; sempre que é um novembro úmido e chuvoso em minha alma; sempre que me pego involuntariamente parando diante de armazéns de caixões e seguindo atrás de todos os funerais que encontro;\n\ne especialmente sempre que meus humores sombrios assumem tal domínio sobre mim, que é necessário um forte princípio moral para impedir-me de deliberadamente entrar na rua e metodicamente derrubar os chapéus das pessoas—\n\nentão considero que é mais do que hora de ir para o mar o mais rápido possível.\n\nEste é o meu substituto para pistola e bala.\n\nCom um floreio filosófico, Catão lança-se sobre sua espada; eu, tranquilamente, tomo o navio.\n\nNão há nada de surpreendente nisso.\n\nSe apenas o soubessem, quase todos os homens, em algum grau, em um momento ou outro, nutrem sentimentos muito semelhantes aos meus em relação ao oceano.",
                 "image_url" => "https://ik.imagekit.io/9azlerk2s/Call%20me%20Ishmael1.png",
                 "audio_url" => "https://ik.imagekit.io/9azlerk2s/Call%20me%20Ishmael.mp3",
                 "duration" => 57,
                 "level" => LessonLevelEnum::Advanced,
                 "category_id" => 1,
-                "source" => "Mobby Dick"
+                "source" => "Mobby Dick",
+                "is_free" => true
+            ],
+            [
+                "title" => "Metamorphosis",
+                "description" => "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.",
+                "text" => "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.\n\nHe lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.\n\nHis many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. “What’s happened to me?” he thought. It wasn’t a dream.\n\nHis room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection of textile samples lay spread out on the table, Samsa was a travelling salesman, and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame.\n\nIt showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer.",
+                "translation" => "Uma manhã, quando Gregor Samsa acordou de sonhos inquietos, encontrou-se transformado em sua cama em um horrível inseto.\n\nEle jazia sobre o dorso duro como uma armadura e, ao erguer um pouco a cabeça, podia ver seu ventre marrom, levemente abaulado e dividido por arcos em rígidas seções. A roupa de cama mal conseguia cobri-lo e parecia prestes a escorregar a qualquer momento.\n\nSuas muitas pernas, lamentavelmente finas em comparação com o tamanho do resto do corpo, agitavam-se desamparadas diante de seus olhos. “O que aconteceu comigo?”, pensou. Não era um sonho.\n\nSeu quarto, um quarto humano comum, embora um pouco pequeno demais, permanecia tranquilamente entre suas quatro paredes familiares. Uma coleção de amostras de tecidos estava espalhada sobre a mesa, Samsa era um caixeiro-viajante, e, acima dela, pendia um quadro que ele havia recortado recentemente de uma revista ilustrada e colocado em uma bonita moldura dourada.\n\nEle mostrava uma dama vestida com um chapéu de pele e uma estola de pele, sentada ereta, erguendo em direção ao observador um pesado regalo de pele que cobria todo o seu antebraço.",
+                "image_url" => "https://ik.imagekit.io/9azlerk2s/Metamorphosis.png",
+                "audio_url" => "https://ik.imagekit.io/9azlerk2s/Metamorphosis.mp3",
+                "duration" => 52,
+                "level" => LessonLevelEnum::Advanced,
+                "category_id" => 1,
+                "source" => "Metamorphosis",
+                "is_free" => false
+            ],
+            [
+                "title" => "Leaving Norland",
+                "description" => "Mrs. Dashwood stayed at Norland for a few months. At first, every place there made her very sad. But after some time, she felt a little better and wanted to leave.",
+                "text" => "Mrs. Dashwood stayed at Norland for a few months. At first, every place there made her very sad. But after some time, she felt a little better and wanted to leave. \n\nShe tried hard to find a new house near Norland, because she did not want to go far away from a place she loved.\n\nHowever, she could not find a house that was both comfortable and not too expensive. \n\nHer eldest daughter was more careful and sensible. She said no to many houses because they were too big and cost too much money, even though her mother liked them.",
+                "translation" => "A Sra. Dashwood ficou em Norland por alguns meses. No começo, cada lugar ali a deixava muito triste. Mas depois de algum tempo, ela se sentiu um pouco melhor e quis ir embora. \n\nEla se esforçou para encontrar uma nova casa perto de Norland, porque não queria ficar longe de um lugar que amava.\n\nNo entanto, ela não conseguiu encontrar uma casa que fosse confortável e não fosse muito cara. \n\nSua filha mais velha era mais cuidadosa e sensata. Ela recusou muitas casas porque eram grandes demais e custavam muito dinheiro, mesmo que sua mãe gostasse delas.",
+                "image_url" => "https://ik.imagekit.io/9azlerk2s/Leaving%20Norland.png",
+                "audio_url" => "https://ik.imagekit.io/9azlerk2s/Leaving%20Norland.mp3",
+                "duration" => 33,
+                "level" => LessonLevelEnum::Beginner,
+                "category_id" => 1,
+                "source" => "Sense and Sensibility",
+                "is_free" => false
+            ],
+            [
+                "title" => "A Chance Meeting on a Foggy Train",
+                "description" => "At the end of November, during a warmer day, a train on the Warsaw and Petersburg railway was getting close to the city at high speed. The morning was very damp and foggy",
+                "text" => "At the end of November, during a warmer day, a train on the Warsaw and Petersburg railway was getting close to the city at high speed. The morning was very damp and foggy, and it was hard for the daylight to appear. People could not see more than a few meters from the train windows.\n\nSome passengers on the train were coming back from other countries. However, the third-class carriages were the most crowded. They were filled with ordinary people of different jobs and backgrounds who had entered the train at stations near the city. Everyone looked tired. Many had sleepy eyes and were cold. Their faces even seemed to have the same pale color as the fog outside.\n\nWhen the day became a little brighter, two passengers in one of the third-class carriages were sitting across from each other. Both were young men, and both wore simple, worn clothes. They had interesting faces, and it was clear that both wanted to start a conversation.\n\nIf they had known how unusual they really were at that moment, they would probably have been surprised by the strange chance that made them sit across from each other in that third-class carriage.",
+                "translation" => "No final de novembro, durante um dia mais quente, um trem da ferrovia Varsóvia–Petersburgo estava se aproximando da cidade em alta velocidade. A manhã estava muito úmida e com neblina, e era difícil a luz do dia aparecer. As pessoas não conseguiam ver mais do que alguns metros pelas janelas do trem.\n\nAlguns passageiros estavam voltando de outros países. No entanto, os vagões de terceira classe estavam os mais cheios. Eles estavam ocupados por pessoas comuns, de diferentes profissões e origens, que haviam embarcado nas estações mais próximas da cidade. Todos pareciam cansados. Muitos tinham olhos sonolentos e estavam com frio. Seus rostos até pareciam ter a mesma cor pálida da neblina do lado de fora.\n\nQuando o dia ficou um pouco mais claro, dois passageiros em um dos vagões de terceira classe estavam sentados um de frente para o outro. Ambos eram jovens, e ambos usavam roupas simples e gastas. Eles tinham rostos interessantes, e era claro que os dois queriam começar uma conversa.\n\nSe soubessem o quão incomuns realmente eram naquele momento, provavelmente ficariam surpresos com o estranho acaso que os fez sentar um de frente para o outro naquele vagão de terceira classe.",
+                "image_url" => "https://ik.imagekit.io/9azlerk2s/A%20Chance%20Meeting%20on%20a%20Foggy%20Train.png",
+                "audio_url" => "https://ik.imagekit.io/9azlerk2s/A%20Chance%20Meeting%20on%20a%20Foggy%20Train.mp3",
+                "duration" => 66,
+                "level" => LessonLevelEnum::Intermediate,
+                "category_id" => 1,
+                "source" => "The idiot",
+                "is_free" => false
             ],
         ];
 
